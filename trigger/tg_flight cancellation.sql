f794bac5f79833bab81d06f49fd4c2a487f771cb
Murday Bhovanen <143430590+jevin31@users.noreply.github.com>
2024-03-13 21:07:42 +0400

    Add files via upload

---
+++ trigger/tg_flight cancellation.sql
@@ -0,0 +1,77 @@
+--Flight cancellation.
+CREATE TRIGGER tg_flight_cancellation
+ON flight
+INSTEAD OF DELETE
+AS
+BEGIN
+	DECLARE @flight_id AS INTEGER;
+
+--Retrieve values deleted.
+	SELECT  @flight_id = flight_id
+	FROM deleted;
+
+--If a flight is deleted.
+	IF @@ROWCOUNT > 0 
+		BEGIN
+
+			DECLARE @booking_id INTEGER,@date DATE,@seat_number INTEGER,@passenger_id INTEGER;
+			
+			--Declare cursor.
+			DECLARE my_cursor CURSOR FOR
+			SELECT booking_id, date, seat_number, passenger_id
+			FROM booking
+			WHERE flight_id = @flight_id;
+
+			--Open cursor
+			OPEN my_cursor
+
+			--Fetch rows one by one and process them.
+			FETCH NEXT FROM my_cursor INTO @booking_id, @date , @seat_number, @passenger_id
+
+			WHILE @@FETCH_STATUS = 0 
+			BEGIN
+				--Insert the bookings that will be refund into booking_refund table//processed fetched row.
+				INSERT INTO booking_refund VALUES(@booking_id, @date, @seat_number, @passenger_id, @flight_id);
+
+				--Delete each row in booking table for flight canceled.
+				DELETE FROM booking WHERE booking_id = @booking_id;
+
+				--Fecth next row.
+				FETCH NEXT FROM my_cursor INTO @booking_id,@date, @seat_number, @passenger_id
+			END;
+
+			--Close cursor.
+			CLOSE my_cursor;
+			DEALLOCATE my_cursor;
+
+			--Delete the flight from flight table.
+			DELETE FROM flight WHERE flight_id = @flight_id;
+
+		END;
+	ELSE
+		PRINT 'Error!'
+END;
+
+--Create Table booking_refund
+CREATE TABLE booking_refund
+(
+    booking_id   INTEGER PRIMARY KEY,
+    date         DATE,
+    seat_number INTEGER,
+    passenger_id INTEGER,
+    flight_id    INTEGER
+);
+
+ALTER TABLE booking_refund
+    ADD FOREIGN KEY (passenger_id) REFERENCES passenger (passenger_id);
+ALTER TABLE booking_refund
+    ADD FOREIGN KEY (seat_number) REFERENCES seat (seat_number);
+
+--Testing
+SELECT * FROM flight;
+SELECT * FROM booking_refund;
+SELECT * FROM booking;
+DELETE FROM flight WHERE flight_id = 1;
+SELECT * FROM flight;
+SELECT * FROM booking_refund;
+SELECT * FROM booking;
